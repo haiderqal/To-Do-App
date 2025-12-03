afe73fe22891a7c517f9f27956472f0336b57379
haiderqal <haideralime@icloud.com>
2025-12-02 19:42:47 -0500

    This is the new commit

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 const inputSize = document.getElementById("inputBox");
+const alertH = document.getElementById("maxAlert");
 
-inputSize.addEventListener("input", function () {
-  inputSize.style.height = "auto";
-  inputSize.style.height = inputSize.scrollHeight + "px";
-});
+// inputSize.addEventListener("input", function () {
+//   inputSize.style.height = "auto";
+//   inputSize.style.height = inputSize.scrollHeight + "px";
+// });
 
+inputSize.addEventListener("input", limitText);
 
-function limitText(value) {
-  if (inputSize.value.length > 100) {
-    
+function limitText() {
+  console.log("Current length:", inputSize.value.length);
+
+  if (inputSize.value.length >= 100) {
+    alertH.style.visibility = "visible";
+  } else {
+    alertH.style.visibility = "hidden";
   }
 }
